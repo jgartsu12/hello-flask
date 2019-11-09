7288752a755eea10fdb85163884adf66261bbf7d
jgartsu12 <jgartsu12@gmail.com>
2019-11-09 16:51:56 -0500

    generated sqllite db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -6,15 +6,13 @@ import os
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'app.sqlite')
-    # make possible to set up sql lite db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-# inherit from model ... only ref primary key for look up #
-class Guide(db.Model)
+class Guide(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title =db.Column(db.String(100), unique=False)
+    title = db.Column(db.String(100), unique=False)
     content = db.Column(db.String(144), unique=False)
 
     def __init__(self, title, content):
@@ -23,19 +21,13 @@ class Guide(db.Model)
 
 
 class GuideSchema(ma.Schema):
-    class Meta:# use tuple to expose fields we want access to
+    class Meta:
         fields = ('title', 'content')
-# instantiate guide schema
-# single guide
-guide_schema = GuideSchema()
-        guides_schema = GuideSchema(many=True)
 
-''' dont need since making our own API endpt
-@app.route('/')
-def hello():
-    return "Hey Flask"
-'''
+
+guide_schema = GuideSchema()
+guides_schema = GuideSchema(many=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
