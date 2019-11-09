d86e921be21426731b2b01624d3b9c0116afa27d
jgartsu12 <jgartsu12@gmail.com>
2019-11-09 17:28:23 -0500

    notes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -28,6 +28,20 @@ class GuideSchema(ma.Schema):
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
+# Endpt to create a new guide
+@app.route('/guide', methods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
 
+    new_guide = Guide(title, content)
+
+    db.session.add(new_guide)
+    db.session.commit() #from sql alchmy opening new conenction to db to save data inside of it
+
+    guide = Guide.query.get(new_guide.id) # query to pass in id to be stored inside gudie var
+
+    return guide_schema.jsonify(guide)
+    
 if __name__ == '__main__':
     app.run(debug=True)
